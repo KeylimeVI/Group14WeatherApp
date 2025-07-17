a94189efdedab5f19774cf5dc2ba2b6c6e607d62
ShushuZhu12 <shushu.xy.zhu@gmail.com>
2025-07-17 18:55:52 -0400

    user

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,22 +1,7 @@
 package entity;
 
-public class User {
-	private String id;
-	private String name;
+public interface User {
+    String getName();
 
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
+    String getPassword();
 }

439b9d01550a323e5ef9ccf02f0cd78c0cbd2833
ShushuZhu12 <shushu.xy.zhu@gmail.com>
2025-07-30 22:55:59 -0400

    moved docs to LogInController interface

--- src/main/java/use_case/log_in/LogInCase.java
+++ src/main/java/use_case/log_in/LogInCase.java
@@ -2,12 +2,6 @@ package use_case.log_in;
 
 public class LogInCase implements LogInController {
 	
-	/**
-	 * Attempts to log in with the given username and password Strings.
-	 * @param username
-	 * @param password
-	 * @return true if log in was successful, otherwise return false
-	 */
 	@Override
 	public boolean attemptLogIn(String username, String password) {
 		// TODO: access use_case of the software here for actual login
--- src/main/java/use_case/log_in/LogInController.java
+++ src/main/java/use_case/log_in/LogInController.java
@@ -2,5 +2,11 @@ package use_case.log_in;
 
 public interface LogInController {
 
+	/**
+	 * Attempts to log in with the given username and password Strings.
+	 * @param username
+	 * @param password
+	 * @return true if log in was successful, otherwise return false
+	 */
 	public boolean attemptLogIn(String username, String password);
 }

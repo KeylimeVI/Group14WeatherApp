b3f4e80ccfdc714287ba16aedab79688ed8a5443
ShushuZhu12 <shushu.xy.zhu@gmail.com>
2025-08-14 16:45:51 -0400

    removed useless

--- src/main/java/use_case/log_in/LogInCase.java
+++ src/main/java/use_case/log_in/LogInCase.java
@@ -6,10 +6,6 @@ public class LogInCase implements LogInController {
 	
 	@Override
 	public boolean attemptLogIn(String username, String password) {
-		// TODO: access use_case of the software here for actual login
-		// this is just a place holder to print some stuff as an example
-		System.out.println("You are:\t\t" + username);
-		System.out.println("Your password is:\t" + "*".repeat(password.length()));
 
 		if (UserHandler.exist(username)) {
 			if (UserHandler.passwordCorrect(username, password)) {
--- src/main/java/use_case/sign_up/SignUpCase.java
+++ src/main/java/use_case/sign_up/SignUpCase.java
@@ -5,7 +5,6 @@ import user_handler.UserHandler;
 public class SignUpCase implements SignUpController {
 	@Override
 	public boolean attemptSignUp(String username, String password) {
-		// TODO Auto-generated method stub
 
 		if (UserHandler.exist(username)) {
 			System.out.println("user already exist");
